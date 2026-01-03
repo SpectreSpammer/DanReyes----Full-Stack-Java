0576b3d59afe4c3eeff1310220b0d7b26dac294e
SpectreSpammer <rnavarra.business@gmail.com>
2026-01-03 15:58:16 +0800

    Jan 3 - Day 6 Accomplishments → Day 7 Methods -> Day 8 OOP

---
+++ src/main/java/day8_OOP/polymorphism/FullTimeEmployeePoly.java
@@ -0,0 +1,38 @@
+package day8_OOP.polymorphism;
+
+public class FullTimeEmployeePoly implements EmployeeInterfacePoly {
+
+    private String name;
+    private int id;
+    private double salary;
+    private double bonus;
+
+
+    public FullTimeEmployeePoly(String name, int id, double salary, double bonus) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public double salary() {
+        return salary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary + bonus;
+    }
+}
