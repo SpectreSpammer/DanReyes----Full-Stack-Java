e607e3a2cf87426851bd63353e5f4cce05deec09
SpectreSpammer <rnavarra.business@gmail.com>
2026-02-15 21:46:13 +0800

    Feb 15 - Day 11  Accomplishments → Java Assessment

---
+++ src/main/java/java_backend_assessment/exception/DuplicateEmployeeException.java
@@ -0,0 +1,7 @@
+package java_backend_assessment.exception;
+
+public class DuplicateEmployeeException extends Exception {
+    public DuplicateEmployeeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/java_backend_assessment/exception/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package java_backend_assessment.exception;
+
+public class EmployeeNotFoundException extends Exception{
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
